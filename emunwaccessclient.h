5e587f5cb5178dd491ac135ea0c2bd0250aeb569
Sylvain Colinet <scolinet@gmail.com>
2023-05-30 09:59:08 +0200

    Update emunwaccessclient.h
    Make bmcPrepareCmd public

--- emunwaccessclient.h
+++ emunwaccessclient.h
@@ -53,6 +53,7 @@ public:
 
     void cmd(const QString& cmd, const QString& args="");
     void bcmd(const QString& cmd, const QString& args, const QByteArray& data);
+    void bcmdPrepare(const QString& cmd, const QString& args, const int datalength);
 
     void cmdEmulatorInfo() { cmd("EMULATOR_INFO"); }
     void cmdEmulationStatus() { cmd("EMULATION_STATUS"); }
@@ -96,7 +97,6 @@ private:
     QQueue<QString> _sent;
 
 protected:
-    void bcmdPrepare(const QString& cmd, const QString& args, const int datalength);
     void bcmdData(const QByteArray& data);
 
 signals:
